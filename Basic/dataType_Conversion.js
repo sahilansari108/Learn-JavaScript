aa14c898b6e286013293c031a4f80e40f27478c3
Md Sahil Ansari <mdsahilansarigrd@gmail.com>
2026-04-13 15:44:00 +0000

    conversion

--- Basic/dataType_Conversion.js
+++ Basic/dataType_Conversion.js
@@ -1,19 +1,37 @@
-let score = "3";
-let scoreNum = Number(score);
-console.log(score, scoreNum);
-console.log(typeof score, typeof scoreNum);
+// let score = "3";
+// let scoreNum = Number(score);
+// console.log(score, scoreNum);
+// console.log(typeof score, typeof scoreNum);
 
 
-let id = "245ab";
-let idNum = Number(id);
-console.log(id, idNum);               // idNum shows NaN 
-console.log(typeof id, typeof idNum); // but idNum Typeof show number
+// let id = "245ab";
+// let idNum = Number(id);
+// console.log(id, idNum);               // idNum shows NaN 
+// console.log(typeof id, typeof idNum); // but idNum Typeof show number
 
-let num = null;
-console.log(num, typeof num);
+// let num = null;
+// console.log(num, typeof num);
 
-let num1 = Number(num);
-console.log(num1, typeof num1);
+// let num1 = Number(num);
+// console.log(num1, typeof num1);
 
-let ud = undefined;
-console.log(ud, typeof ud);
+// let ud = undefined;
+// console.log(ud, typeof ud);
+
+// let name = "sahil";
+// let namenum = Number(name);
+// console.log(namenum, typeof namenum);
+
+// let flag = 1;
+// let flagNum = Boolean(flag);
+// console.log(flagNum, typeof flagNum);
+
+// let flag1 = "sahil";
+// let flagNum1 = Boolean(flag1);
+// console.log(flagNum1, typeof flagNum1);
+
+let num5 = 50;
+let str5 = String(num5);
+let bool5 = Boolean(num5);
+console.log(str5, typeof str5);
+console.log(bool5, typeof bool5);
