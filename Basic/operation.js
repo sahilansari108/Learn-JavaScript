b01d97324bcb78725b733b9480cc9326e47c766f
Md Sahil Ansari <mdsahilansarigrd@gmail.com>
2026-04-13 15:58:29 +0000

    operation on js

---
+++ Basic/operation.js
@@ -0,0 +1,17 @@
+let value = 2;
+let negvalue = -value;
+console.log(negvalue);
+
+let str1="sahil ";
+let str2 = "ansari";
+let str3 = str1+str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1"+2+3);
+console.log(1 +2 + "3");
+
+console.log(+true);         // --> 1
+// console.log(true+);     --> error
+console.log(+"");           // --> 0
