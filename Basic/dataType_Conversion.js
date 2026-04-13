ecc23e7ed32a0f38661f6bf51de86e4cd41ced01
Md Sahil Ansari <mdsahilansarigrd@gmail.com>
2026-04-13 13:30:27 +0000

    dataType_conversion

---
+++ Basic/dataType_Conversion.js
@@ -0,0 +1,19 @@
+let score = "3";
+let scoreNum = Number(score);
+console.log(score, scoreNum);
+console.log(typeof score, typeof scoreNum);
+
+
+let id = "245ab";
+let idNum = Number(id);
+console.log(id, idNum);               // idNum shows NaN 
+console.log(typeof id, typeof idNum); // but idNum Typeof show number
+
+let num = null;
+console.log(num, typeof num);
+
+let num1 = Number(num);
+console.log(num1, typeof num1);
+
+let ud = undefined;
+console.log(ud, typeof ud);
