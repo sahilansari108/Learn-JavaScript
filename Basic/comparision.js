fa72edff2c47500b44520ee610439bf9c1399a4f
Md Sahil Ansari <mdsahilansarigrd@gmail.com>
2026-04-13 16:10:11 +0000

    comparision in JS

---
+++ Basic/comparision.js
@@ -0,0 +1,15 @@
+ console.log("00">1);
+ console.log("02">1);
+
+ console.log(null > 0);
+ console.log(null >= 0);
+ console.log(null == 0);
+ 
+// null sometime converted in NaN or sometime to 0
+
+ console.log(undefined > 0);
+ console.log(undefined >= 0);
+ console.log(undefined == 0);
+
+
+console.log("2" === 2);
