8561a2a2f82185226a0735928f95fabe5c9e9e6a
Md Sahil Ansari <mdsahilansarigrd@gmail.com>
2026-04-13 13:07:42 +0000

    Learning Variables

---
+++ Basic/variableDeclaration.js
@@ -0,0 +1,11 @@
+const accountId = 23030108;
+let accountEmail = "sahil@gmail.com";
+var accountPassWord = 123456;
+accountCity = "jaipur";
+// accountId = 23030154; --> Not allowed
+accountPassWord = 2983158;
+accountEmail = "sahilansari@gmail.com";
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassWord, accountCity]);
+let accountState;
+console.log(accountState);
