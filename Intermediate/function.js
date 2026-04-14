e171892c4ee86db80f7df877fc163522c0a0b4dd
Md Sahil Ansari <mdsahilansarigrd@gmail.com>
2026-04-14 16:34:25 +0000

    function

--- Intermediate/function.js
+++ Intermediate/function.js
@@ -22,7 +22,42 @@ function login(userName){
     }
     return `${userName} just logged in`
 }
-console.log(login("sahil"));
-console.log(login());
-let temp = login("sahil ansari");
-console.log(temp);
+// console.log(login("sahil"));
+// console.log(login());
+// let temp = login("sahil ansari");
+// console.log(temp);
+
+function calculateCarPrice(val1, val2, ...num1){        // rest operator it bundled the remaining parameter in array
+    return num1;                                
+}
+// console.log(calculateCarPrice(200, 400, 500, 800, 900));
+
+
+const user = {
+    userName : "sahil",
+    coursePrice : 199
+}
+
+function handleObject(anyobject){
+    console.log(`user Name is ${anyobject.userName} and price is ${anyobject.coursePrice}`)
+}
+
+handleObject(user);
+
+handleObject({               // directly pass object as argument
+    userName : "sameer",
+    coursePrice : 2399,
+})
+
+
+const arr = [100,200,300];
+
+function handleArray(anyArr){
+    return anyArr[1];
+}
+
+// console.log(handleArray(arr));
+console.log(handleArray([10,20,30,4,50]));
+
+
+
