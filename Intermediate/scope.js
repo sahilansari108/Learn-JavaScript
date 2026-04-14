3fe2d24bf7b63e5529e55a85a5c4920feb0035d3
Md Sahil Ansari <mdsahilansarigrd@gmail.com>
2026-04-14 16:41:44 +0000

    scope

---
+++ Intermediate/scope.js
@@ -0,0 +1,19 @@
+let a = 100;
+const b = 200;
+var c = 300;
+var d = 400;
+
+if(true){
+    let a = 10;
+    const b = 20;
+    var c = 30;
+    d = 40;
+}
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+
