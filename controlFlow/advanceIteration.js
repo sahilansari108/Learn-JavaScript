a9284c035e1f9b36cc3f38f0436bfeaba307302f
Md Sahil Ansari <mdsahilansarigrd@gmail.com>
2026-04-16 06:24:00 +0000

    Reeduce Function

--- controlFlow/advanceIteration.js
+++ controlFlow/advanceIteration.js
@@ -10,6 +10,6 @@ const newNums1 = myNumber.map( (num) => {return num + 15})
 const newNums2 = myNumber.map( (num) => (num+2)).map( (num) => (num+4))   //first map array(but not store on newNums2) made then second map apply
 // console.log(newNums2);
 
-const newNums3 = myNumber.map( (num) => num*7).filter((val) => val>=35)
+const newNums3 = myNumber.map( (num) => num*7).filter((val) => val>=35)   // we can add more method through chaining
 console.log(newNums3);
 
---
+++ controlFlow/arrayReduce.js
@@ -0,0 +1,34 @@
+const myNumber = [1,2,3,4];
+const total = myNumber.reduce(function(acc,currval){
+    return acc+currval;
+},5)                        // here 5 is the initial value pass to accumulator
+// console.log(total);
+
+
+const myTotal = myNumber.reduce((acc, currval) => (currval + acc),0);
+// console.log(myTotal);
+
+
+// reduce with array object
+
+const myCart = [
+    {
+        courseName : "c++",
+        price : 2999
+    },
+    {
+        courseName : "java",
+        price : 3999
+    },
+    {
+        courseName : "web-Dev",
+        price : 4999,
+    },
+    {
+        courseName : "python",
+        price : 1999,
+    },
+]
+
+const cartTotal = myCart.reduce((acc, ele) => (ele.price + acc),0);
+console.log(cartTotal);
