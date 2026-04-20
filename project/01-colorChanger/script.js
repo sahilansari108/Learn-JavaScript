3ff2b3562d3ffa6d5111a11121c7a641f6e6afdc
Md Sahil Ansari <mdsahilansarigrd@gmail.com>
2026-04-20 05:48:55 +0000

    color changer

---
+++ project/01-colorChanger/script.js
@@ -0,0 +1,22 @@
+let body = document.querySelector('body');
+const buttons = document.querySelectorAll('.button')
+
+buttons.forEach(function(button){
+    console.log(button);
+    button.addEventListener('click', function(e){
+        console.log(e);
+        console.log(e.target);
+        if(e.target.id === 'grey'){
+            body.style.backgroundColor = e.target.id;     // or 'grey'
+        }
+        else if(e.target.id === 'white'){
+            body.style.backgroundColor = e.target.id;     // or 'white'
+        }
+        else if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id;     // or 'blue'
+        }
+        else if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id;     // or 'yellow'
+        }
+    })
+})
