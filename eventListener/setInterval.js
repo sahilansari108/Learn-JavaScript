eae4919b8bfdd4db1dc2f3d227bd5c76a3f0beac
Md Sahil Ansari <mdsahilansarigrd@gmail.com>
2026-04-27 17:50:57 +0000

    set interval

--- eventListener/setInterval.js
+++ eventListener/setInterval.js
@@ -1,4 +1,19 @@
-const changeText(){
-    console.log("heading Changed");
-    document.querySelector('#start').addEventListener()
+
+let intervalid;
+const startEvent = function(){
+    function print(){
+        console.log("sahil",Date.now());
+        document.querySelector('h1').innerHTML = "Sahil Time Now : "+Date.now();
+    }
+    if(!intervalid){
+        intervalid = setInterval(print, 1000);
+    }
+    
+    
 }
+const stopEvent = function(){
+    clearInterval(intervalid);
+    intervalid = null;
+}
+document.querySelector('#start').addEventListener('click',startEvent);
+document.querySelector('#stop').addEventListener('click',stopEvent)
